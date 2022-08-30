14689535ec8153ce5b3a8519f2c6e94d13301edd
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-30 09:51:55 +0300

    Update the website domain

--- config/appInfo.js
+++ config/appInfo.js
@@ -1,11 +1,18 @@
 let websiteDomain;
 const apiBasePath = '/api/auth/'
 
+<<<<<<< master
 if (process.env.NODE_ENV == "production") {
   websiteDomain = process.env.VERCEL_URL
 } else {
   websiteDomain = "http://localhost:3000"
 }
+=======
+export const websiteDomain =
+  process.env.VERCEL_URL ||
+  process.env.NEXT_PUBLIC_APP_URL ||
+  `http://localhost:${port}`
+>>>>>>> Update the website domain
 
 const appInfo = {
   appName: 'SuperTokens Demo App',
